2cd27cb4087abdb78a36c1f203dcdca6ff19cc93
Victor <victorferrari13@gmail.com>
2018-05-05 16:59:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/victo/acorde/AcordeActivity.java
@@ -0,0 +1,13 @@
+package com.example.victo.acorde;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcordeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acorde);
+    }
+}
